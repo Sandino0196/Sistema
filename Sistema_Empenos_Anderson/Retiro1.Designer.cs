8cf99252572d5b37b5c9521c8302e3bb37743526
Dell <Dell@MERMAIDKELS>
2019-06-22 00:19:49 -0600

    Retiro Prueba

---
+++ Sistema_Empenos_Anderson/Retiro1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Sistema_Empenos_Anderson
+{
+    partial class Retiro1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Retiro1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 306);
+            this.Name = "Retiro1";
+            this.Text = "Retiro1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
