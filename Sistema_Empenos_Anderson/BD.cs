603749b6a944b2dfbdf541404cf6282a3dcc6eff
Sandino <sandinorodrigo@gmail.com>
2019-07-12 13:37:18 -0600

    Cambio Forzado 2

--- Sistema_Empenos_Anderson/BD.cs
+++ Sistema_Empenos_Anderson/BD.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Threading.Tasks;
 
 namespace Sistema_Empenos_Anderson
 {
--- Sistema_Empenos_Anderson/Usuario.cs
+++ Sistema_Empenos_Anderson/Usuario.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sistema_Empenos_Anderson
+﻿namespace Sistema_Empenos_Anderson
 {
     public class Usuario
     {
