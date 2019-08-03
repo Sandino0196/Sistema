eb7620bfae73a2f7a7ef3ecc5be4a5650cb0fc78
Oscar Flores <oflores482@gmail.com>
2019-08-03 02:21:56 -0600

    Cambiios que me pide esta cosa

--- Sistema_Empenos_Anderson/BD.cs
+++ Sistema_Empenos_Anderson/BD.cs
@@ -11,7 +11,7 @@ namespace Sistema_Empenos_Anderson
 
         public static void OpenConnection()
         {
-            connection.ConnectionString = @"Data Source=DESKTOP-D1B8U5J; Initial Catalog=Base_Empeños; Integrated Security=Yes";
+            connection.ConnectionString = @"Data Source=DESKTOP-T785USI; Initial Catalog=Base_Empeños; Integrated Security=Yes";
             connection.Open();
             //Donde dice DATA SOURCE le ponen el nombre de su máquina; 
         }
